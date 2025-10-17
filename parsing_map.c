aa276eb82111f086bc346cfb783a527ca9c24109
HOUDA-KEMMOUNE <houdakemmoune2006@gmail.com>
2025-10-17 16:04:04 +0100

    Adding the paths

--- parsing_map.c
+++ parsing_map.c
@@ -12,17 +12,41 @@
 
 #include "cub.h"
 
-static int	first_word(char *word, int *count)
-{	
+static int	check_path(char *path)
+{
+	int	fd;
+
+	fd = open(path, O_RDWR);
+	printf("path -> %s\n", path);
+	if (fd < 0)
+	{
+		printf("fd => %d || Path doesn't exist :/\n", fd);
+		return (0);
+	}
+	return (1);
+}
+
+static int	first_word(char *word, char *path, int *count)
+{
+	char	*new_path;
+
 	if ((ft_strncmp(word, "NO", 2) == 0) || (ft_strncmp(word, "SO", 2) == 0)
 		|| (ft_strncmp(word, "WE", 2) == 0) || (ft_strncmp(word, "EA", 2) == 0))
 	{
-		// printf("The word is: %s\n", word);
+		new_path = ft_strtrim(path, "./");
+		printf("new path: %s\n", new_path);
+
+		if (check_path(new_path) == 0)
+		{
+			exit (1);
+		}
 		(*count)++;
+		free (new_path);
 		return ((*count));
 	}
 	return (0);
 }
+
 
 int	parsing_map(int fd)
 {
@@ -36,10 +60,13 @@ int	parsing_map(int fd)
 	while ((line = get_next_line(fd)) != NULL)
 	{
 		s = ft_split(line, ' ');
-		if (first_word(s[0], &count) == 0)
+		if (first_word(s[0], s[1], &count) == 0)
 		{
+			// check_path(s[1]);
 			if (count == 4)
+			{
 				exit (0);
+			}
 		}
 	}
 	if (count != 4)
