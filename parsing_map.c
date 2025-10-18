ff971a1a445608dc72aade7ee97107cdf1673fdd
HOUDA-KEMMOUNE <houdakemmoune2006@gmail.com>
2025-10-18 13:42:31 +0100

    First move done in the parsing

--- parsing_map.c
+++ parsing_map.c
@@ -16,11 +16,16 @@ static int	check_path(char *path)
 {
 	int	fd;
 
-	fd = open(path, O_RDWR);
-	printf("path -> %s\n", path);
+	if (path[0] == '\0')
+	{
+		printf("Write something in the path please :/\n");
+		return (0);
+	}
+	fd = open(path, O_RDONLY);
+	// printf("path -> {%s}\n", path);
 	if (fd < 0)
 	{
-		printf("fd => %d || Path doesn't exist :/\n", fd);
+		printf("Path doesn't exist :/\n");
 		return (0);
 	}
 	return (1);
@@ -28,20 +33,18 @@ static int	check_path(char *path)
 
 static int	first_word(char *word, char *path, int *count)
 {
-	char	*new_path;
+	// char	*new_path;
 
 	if ((ft_strncmp(word, "NO", 2) == 0) || (ft_strncmp(word, "SO", 2) == 0)
 		|| (ft_strncmp(word, "WE", 2) == 0) || (ft_strncmp(word, "EA", 2) == 0))
 	{
-		new_path = ft_strtrim(path, "./");
-		printf("new path: %s\n", new_path);
-
-		if (check_path(new_path) == 0)
+		// new_path = ft_strtrim(path, "./");
+		if (check_path(path) == 0)
 		{
 			exit (1);
 		}
 		(*count)++;
-		free (new_path);
+		free (path);
 		return ((*count));
 	}
 	return (0);
@@ -54,15 +57,18 @@ int	parsing_map(int fd)
 	char	*line;
 	char	**s;
 	int		i;
+	char	*new_path;
 
 	i = 0;
 	count = 0;
 	while ((line = get_next_line(fd)) != NULL)
 	{
 		s = ft_split(line, ' ');
-		if (first_word(s[0], s[1], &count) == 0)
+		new_path = ft_strtrim(s[1], "\n");
+		// if (new_path[0] == '\0')
+		// 	printf("new path --> {%s}\n", new_path);
+		if (first_word(s[0], new_path, &count) == 0)
 		{
-			// check_path(s[1]);
 			if (count == 4)
 			{
 				exit (0);
